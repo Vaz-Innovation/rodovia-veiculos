0b734b61da0beb34d19e7c5b778692c2b0553f0b
Manuel Antunes <manuelnascimento5589@gmail.com>
2026-04-30 12:51:34 -0300

    feat: setup for deployment

---
+++ docker/wordpress/plugins/test/paulinho-bacana.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @wordpress-plugin
+ * Plugin Name:       Paulinho Bacana
+ * Plugin URI:        https://github.com/m-muhsin/wp-graphql-reading-time
+ * Description:       Mt gente fina
+ * Version:           0.0.1
+ * Requires at least: 5.0
+ * Requires PHP:      7.2
+ * Author:            Manuel Antunes
+ * Author URI:        https://manuelantun.es
+ * License:           GNU General Public License v2.0 / MIT License
+ * Text Domain:       paulinho-bacana
+ * Domain Path:       /languages
+ */
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+echo "Olá, eu sou o plugin Paulinho Bacana!";
